4fce3eb24d8454f9b29dbb33f8c7b775e75821a3
unknown <bhambure1994@gmail.com>
2025-09-02 16:27:36 +0530

    added menu

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Calculator
+{
+};
+
+int menuChoice()
+{
+    int choice;
+    cout << "0.Exit" << endl;
+    cin >> choice;
+    return choice;
+}
+
+int main()
+{
+}
