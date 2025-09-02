be3a5eafba78bf69e387e04078630da46bff4a2a
unknown <bhambure1994@gmail.com>
2025-09-02 16:41:47 +0530

    add function added

--- main.cpp
+++ main.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 class Calculator
 {
+private:
+    int num1;
+    int num2;
+    int res;
+
+public:
+    void add(int num1, int num2)
+    {
+        res = num1 + num2;
+    }
 };
 
 int menuChoice()
